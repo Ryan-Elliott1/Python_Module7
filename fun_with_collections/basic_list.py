57cf24e4d0b4b125ecedaf83f87b7d9971ce0d9c
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-21 11:24:51 -0500

    basic list functions

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,5 +1,37 @@
+"""
+Program: basic_list.py
+Author: Ryan Elliott
+Last date modified: 06/21/2020
+This program asks the user to input 3 numbers and then creates a list
+Input numbers
+Outputs the created list
+"""
+
+
 def make_list():
-    pass
+    """Returns the created list
+    :returns full_list
+    """
+    full_list = []
+    for x in range(3):
+        full_list.append(get_input())
+    return full_list
+
 
 def get_input():
-    pass
+    """Returns the users input
+    :returns num
+    """
+    try:
+        num = int(input("Enter a number: "))
+    except ValueError as err:
+        print("ValueError encountered")
+    else:
+        return num
+
+
+if __name__ == '__main__':
+    try:
+        print(make_list())
+    except ValueError as err:
+        print("ValueError encountered")
--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -4,6 +4,6 @@ from fun_with_collections import basic_list
 
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.basic_list.get_input', return_value='4')
+    @patch('fun_with_collections.basic_list.get_input', return_value=3)
     def test_make_list(self, input):
-        self.assertEqual(basic_list.make_list(), [5, 5, 5])
+        self.assertEqual(basic_list.make_list(), [3, 3, 3])
