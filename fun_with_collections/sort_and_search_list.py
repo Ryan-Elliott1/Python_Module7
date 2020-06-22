f17d605218764ee23ba215e8d4f475fd44025468
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-21 19:36:01 -0500

    search tests pass

--- fun_with_collections/sort_and_search_list.py
+++ fun_with_collections/sort_and_search_list.py
@@ -2,16 +2,30 @@
 Program: sort_and_search.py
 Author: Ryan Elliott
 Last date modified: 06/21/2020
-
+This program searches for an object in a list and returns the index and sorts the list
+Input list and the object to search for or the list to sort
+Returns the object index or sorted list
 """
 
 
 def search_list(a_list, obj_look):
-    pass
+    """Returns index if object is present or -1 is not found
+    :param a_list the users list
+    :param obj_look the object to search for
+    :returns object_index or -1
+    """
+    try:
+        obj_index = a_list.index(obj_look)
+    except ValueError as err:
+        return -1
+    else:
+        return obj_index
 
 
 def sort_list(a_list):
     pass
 
 
-
+if __name__ == '__main__':
+    ab_list = [3, 2, 5]
+    print(search_list(ab_list, 4))
