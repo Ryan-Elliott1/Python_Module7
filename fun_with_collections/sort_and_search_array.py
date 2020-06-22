36d43c0295413b54da538cc8c4ff884ea8004fd2
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-21 21:30:48 -0500

    sort_array failed tests

---
+++ fun_with_collections/sort_and_search_array.py
@@ -0,0 +1,18 @@
+"""
+Program: sort_and_search_array.py
+Author: Ryan Elliott
+Last date modified: 06/21/2020
+
+"""
+
+
+def search_array(a_array, obj_look):
+    pass
+
+
+def sort_array(a_array):
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ test_collections/test_sort_and_search_array.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import patch
+from fun_with_collections import sort_and_search_array
+
+
+class TestList(unittest.TestCase):
+    def test_search_array_found(self):
+        self.assertEqual(sort_and_search_array.search_array([3, 2, 5], 5), 2)
+
+    def test_search_array_not_found(self):
+        self.assertEqual(sort_and_search_array.search_array([3, 2, 5], 4), -1)
