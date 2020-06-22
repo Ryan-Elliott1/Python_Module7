c895dfdf759834cb8b0f95a4b3484fde7d83caa3
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-21 19:05:14 -0500

    search_list tests fail

---
+++ fun_with_collections/sort_and_search_list.py
@@ -0,0 +1,17 @@
+"""
+Program: sort_and_search.py
+Author: Ryan Elliott
+Last date modified: 06/21/2020
+
+"""
+
+
+def search_list(a_list, obj_look):
+    pass
+
+
+def sort_list(a_list):
+    pass
+
+
+
---
+++ test_collections/test_sort_and_search.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import patch
+from fun_with_collections import sort_and_search_list
+
+
+class TestList(unittest.TestCase):
+    def test_search_list_found(self):
+        self.assertEqual(sort_and_search_list.search_list([3, 2, 5], 5), 2)
+
+    def test_search_list_not_found(self):
+        self.assertEqual(sort_and_search_list.search_list([3, 2, 5], 4), -1)
