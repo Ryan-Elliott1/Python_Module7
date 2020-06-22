eee5c29f75a94e7206e81c35f2d3c1c5e64f25e5
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-21 21:33:39 -0500

    search array tests pass

--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -4,10 +4,16 @@ Author: Ryan Elliott
 Last date modified: 06/21/2020
 
 """
+import array as arr
 
 
 def search_array(a_array, obj_look):
-    pass
+    try:
+        obj_index = a_array.index(obj_look)
+    except ValueError as err:
+        return -1
+    else:
+        return obj_index
 
 
 def sort_array(a_array):
@@ -15,4 +21,6 @@ def sort_array(a_array):
 
 
 if __name__ == '__main__':
-    pass
+    numbers = [3, 2, 5]
+    numbers_array = arr.array('i', numbers)
+    print(search_array(numbers_array, 5))
